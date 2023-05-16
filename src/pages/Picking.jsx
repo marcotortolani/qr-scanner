d1685fb90c72e7e6aa236f93022bbd9a9cf22c98
Marco Tortolani <mjtorto@gmail.com>
2023-05-16 09:58:37 -0300

    fix cors error

--- src/pages/Picking.jsx
+++ src/pages/Picking.jsx
@@ -126,6 +126,7 @@ export default function Picking() {
           "Access-Control-Allow-Headers":
             "POST, GET, PUT, DELETE, OPTIONS, HEAD, Authorization, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin",
           "Content-Type": "application/json",
+          "mode": "no-cors"
         },
       })
       .then((response) => {
